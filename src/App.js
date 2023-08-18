79895b43f0e4fefe181af631150e8acfc665429d
Jordan Ayotte <jordanayotte13@outlook.com>
2023-08-17 23:20:03 -0400

    secured env vairables, changed coffee pour width and cup to look 3d

--- src/App.js
+++ src/App.js
@@ -47,9 +47,9 @@ function App() {
   return (
     <div id='beans' className="flex flex-col relative items-center py-4">
       <div className="flex py-32">
-      <p1 id='jocator' className='text-white text-4xl mr-2'>The Jocator</p1>
       </div>
       <div className='relative'>
+        {!coffeePouring ? <div>
       <svg className='absolute -top-24 left-16' width="37" height="73" viewBox="0 0 37 73" fill="none" xmlns="http://www.w3.org/2000/svg">
 <path id='steam1' d="M17 70C25.3356 59.2691 32.45 48.6318 33.5 34.6667C34.1024 26.6542 33.7567 14.6575 28.5556 7.88887C22.9745 0.625808 8.56593 0.614571 3.66667 8.88887C1.3775 12.755 2.53133 20.1642 6.94445 22.0555C9.48355 23.1437 17.4573 24.1916 19.7778 22C20.9964 20.8491 14.8499 17.0198 14 16" stroke="white" stroke-width="5" stroke-linecap="round"/>
 </svg>
@@ -59,20 +59,22 @@ function App() {
 <svg className='absolute -top-24 left-52' width="32" height="76" viewBox="0 0 32 76" fill="none" xmlns="http://www.w3.org/2000/svg">
 <path id='steam3' d="M17 73C-2.36732 71.924 0.166263 35.3987 16.3889 31.1667C19.7847 30.2808 24.2843 29.3603 26 25.5C27.3875 22.3781 30.8974 15.765 28.4445 12.6111C22.5689 5.05678 7.8442 19.6044 6.22223 16C1.87647 6.34276 10.9787 7.02129 15 3" stroke="white" stroke-width="5" stroke-linecap="round"/>
 </svg>
+</div> : null}
       <button onClick={() => setCoffeePouring(true) }>
       <img id='cup' className={coffeePouring ? '-rotate-45 transition-all relative z-20' : null} src={coffeeImage} alt='cup of coffee'></img>
       </button>
       {coffeePouring ? 
-      <svg className='absolute top-[9.1rem] -left-[2.0rem] -scale-x-100 z-0' width="116" height="678" viewBox="0 0 116 678" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path id='coffee' d="M86.6808 11C94.7978 31.4175 103.276 49.5778 105.074 72.0015C108.401 113.502 102.81 156.544 87.5619 195.238C70.0514 239.673 44.2297 279.895 28.8591 325.315C11.8922 375.451 7.57152 427.877 11.1821 480.622C13.4825 514.226 19.8859 548.903 34.0906 579.581C41.5613 595.716 52.3342 610.792 58.651 627.407C63.2326 639.457 67.8475 654.966 67.8475 668" stroke="#A16207" stroke-width="20" stroke-linecap="round"/>
-      </svg>      
+      <svg className='absolute top-[9.4rem] -left-[2.1rem] -scale-x-100 z-0' width="106" height="668" viewBox="0 0 106 668" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path id='coffee' d="M81.6808 6C89.7978 26.4175 98.2758 44.5778 100.074 67.0015C103.401 108.502 97.8102 151.544 82.5619 190.238C65.0514 234.673 39.2297 274.895 23.8591 320.315C6.89221 370.451 2.57152 422.877 6.18215 475.622C8.48249 509.226 14.8859 543.903 29.0906 574.581C36.5613 590.716 47.3342 605.792 53.651 622.407C58.2326 634.457 62.8475 649.966 62.8475 663" stroke="#A16207" stroke-width="10" stroke-linecap="round"/>
+      </svg>
+            
        : null}
       </div>
       {error}
-      {coffeePouring ? <div id='coffeedata' className='flex flex-col w-full md:w-[40rem] bg-neutral-950 bg-opacity-80 rounded-lg overflow-hidden absolute -bottom-[54rem] opacity-0 z-20'>
+      {coffeePouring ? <div id='coffeedata' className='flex flex-col w-full md:w-[40rem] border border-neutral-800 bg-neutral-950 bg-opacity-80 rounded-lg overflow-hidden absolute -bottom-[35rem] opacity-0 z-20'>
         <CoffeeShopFinder latitude={latitude} longitude={longitude}/>
       </div> : null}
-      <footer className='absolute -bottom-[110vh] text-white'>Github repo: github.com/blahlbahlbah</footer>
+      <footer className='absolute -bottom-[40rem] text-white'>Github repo: <a className='underline text-blue-500' href='https://github.com/joayo13/findnearestcafe'>https://github.com/joayo13/findnearestcafe</a></footer>
       </div>
   );
 }
--- src/CoffeeShopFinder.js
+++ src/CoffeeShopFinder.js
@@ -3,42 +3,44 @@ import { GoogleApiWrapper } from 'google-maps-react';
 
 const NearestCoffeeShop = ({ google, latitude, longitude }) => {
   const [nearestCoffeeShop, setNearestCoffeeShop] = useState(null);
+  const [hasFetchedData, setHasFetchedData] = useState(false);
 
-  // useEffect(() => {
-  //   const service = new google.maps.places.PlacesService(document.createElement('div'));
+  useEffect(() => {
+    if (!hasFetchedData) {
+      const service = new google.maps.places.PlacesService(document.createElement('div'));
 
-  //   const request = {
-  //     location: new google.maps.LatLng(latitude, longitude),
-  //     rankBy: google.maps.places.RankBy.DISTANCE,
-  //     types: ['cafe'],
-  //   };
+      const request = {
+        location: new google.maps.LatLng(latitude, longitude),
+        rankBy: google.maps.places.RankBy.DISTANCE,
+        types: ['cafe'],
+      };
 
-  //   service.nearbySearch(request, (results, status) => {
-  //     if (status === google.maps.places.PlacesServiceStatus.OK) {
-  //       const coffeeShopWithHighestRating = results.reduce(
-  //         (prev, current) => (current.rating > prev.rating ? current : prev),
-  //         results[0]
-  //       );
-  //       setNearestCoffeeShop(coffeeShopWithHighestRating);
-  //     }
-  //   });
-  // }, [google, latitude, longitude]);
+      service.nearbySearch(request, (results, status) => {
+        if (status === google.maps.places.PlacesServiceStatus.OK) {
+          const cafesWithReviews = results.filter(shop => shop.user_ratings_total > 100);
+          console.log(results)
+          const coffeeShopWithHighestRating = cafesWithReviews.reduce(
+            (prev, current) => (current.rating > prev.rating ? current : prev),
+            cafesWithReviews[0]
+          );
+          setNearestCoffeeShop(coffeeShopWithHighestRating);
+          setHasFetchedData(true); // Set the flag to prevent further fetching
+        }
+      });
+    }
+  }, [google, latitude, longitude, hasFetchedData]);
 
-  // if (!nearestCoffeeShop) {
-  //   return <div>Loading...</div>;
-  // }
+  if (!nearestCoffeeShop) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <div className='flex flex-col items-center text-2xl text-white'>
-      {/* <p>Name: {nearestCoffeeShop.name}</p>
-      <p>Address: {nearestCoffeeShop.vicinity}</p>
-      <p>Rating: {nearestCoffeeShop.rating}</p> */}
-      <div id='beansbanner' className='w-full py-4 shadow-[inset_0_-60px_50px_-20px_rgba(0,0,0,0.5)]'>
-      <h1 className='text-3xl'>Best cup of Joe:</h1>
+      <div id='beansbanner' className='w-full py-4 shadow-[inset_0_-60px_50px_-20px_rgba(0,0,0,0.6)]'>
+      <h1 className='text-3xl'>Best Cafe Near You:</h1>
       </div>
-      <p className='py-4'>Cafe: Opulence Coffee</p>
-      <p className='py-4'>Address: 21 Dalhousie Street</p>
-      <p>Cafe Rating</p>
+      <p className='py-4'>Cafe: {nearestCoffeeShop.name}</p>
+      <p className='py-4'>Address: {nearestCoffeeShop.vicinity}</p>
       <div className='flex relative py-4'>
         {/* stars */}
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="0" stroke="currentColor" class="w-10 h-10">
@@ -56,7 +58,7 @@ const NearestCoffeeShop = ({ google, latitude, longitude }) => {
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="0" stroke="currentColor" class="w-10 h-10">
         <path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
         </svg>
-        <div style={{width: '90%'}} className='flex absolute top-0 left-0 overflow-hidden'>
+        <div style={{width: nearestCoffeeShop.rating * 20 + '%'}} className='flex absolute top-0 left-0 overflow-hidden'>
         {/* stars */}
         <svg xmlns="http://www.w3.org/2000/svg" fill="#A16207" viewBox="0 0 24 24" stroke-width="0" stroke="currentColor" class="w-10 h-10 min-w-max">
         <path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
@@ -80,5 +82,5 @@ const NearestCoffeeShop = ({ google, latitude, longitude }) => {
 };
 
 export default GoogleApiWrapper({
-  apiKey: 'AIzaSyB3OL0yiLGOrDpP2WXT9gI-xDR81_DgLT0',
+  apiKey: process.env.REACT_APP_SECRET_KEY,
 })(NearestCoffeeShop);
